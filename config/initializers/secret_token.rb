ba99a1a5860c3a2f07bb6cb9d78b5dc063ffdc59
Nick Swider <swider@webs.com>
2011-12-02 21:45:26 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountTo270::Application.config.secret_token = '2f6cd7735aa98a440b9edbe7bf3b60ddefbfbc4f93c39653fffbf21d02342e83df9aa7bb4878100c5640d36790e62413acd04ef3dab3e03fa40e6980206bb53c'
